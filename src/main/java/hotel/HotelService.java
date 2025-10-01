5af26a218cd853fae8cbc534dbeea925be0c2d7b
choiseohyun55 <qkr7576@naver.com>
2025-10-01 14:23:18 +0900

    hotel 수정 끝

--- src/main/java/hotel/HotelService.java
+++ src/main/java/hotel/HotelService.java
@@ -1,5 +1,6 @@
 package hotel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HotelService {
@@ -9,6 +10,18 @@ public class HotelService {
     // 전체 호텔 리스트 반환
     public List<Hotel> getAllHotels() {
         return hotelRepository.getAllHotels();
+    }
+    public List<Hotel> getHotelsByCity(String city) {
+        List<Hotel> hotels = hotelRepository.getAllHotels();
+        List<Hotel> result = new ArrayList<>();
+
+        for (Hotel h : hotels) {
+            if (h.getCity().equalsIgnoreCase(city)) {
+                result.add(h);
+            }
+        }
+
+        return result;  // 해당 도시에 있는 호텔 리스트 반환
     }
 
 }
--- src/main/java/test/HotelTest.java
+++ src/main/java/test/HotelTest.java
@@ -8,11 +8,18 @@ import java.util.List;
 public class HotelTest {
     public static void main(String[] args) {
         HotelService hotelService = new HotelService();
-        List<Hotel> hotels = hotelService.getAllHotels();
 
-        System.out.println("호텔 개수: " + hotels.size());
-        for (Hotel h : hotels) {
-            System.out.println(h.getName() + " | " + h.getCity() + " | " + h.getPrice());
+        // 서울 호텔만 조회
+        List<Hotel> seoulHotels = hotelService.getHotelsByCity("Seoul");
+
+        System.out.println("서울 호텔 개수: " + seoulHotels.size());
+        System.out.println("------ 서울 호텔 리스트 ------");
+        for (Hotel h : seoulHotels) {
+            System.out.println("호텔 이름: " + h.getName());
+            System.out.println("도시: " + h.getCity());
+            System.out.println("가격: " + h.getPrice());
+            System.out.println("평점: " + h.getRating());
+            System.out.println("----------------------------");
         }
     }
 }
