d6982f87f82c374bb637de274f2d3bebacceab8b
choiseohyun55 <qkr7576@naver.com>
2025-10-01 14:14:35 +0900

    hotel 수정

---
+++ src/main/java/test/HotelTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import hotel.Hotel;
+import hotel.HotelService;
+
+import java.util.List;
+
+public class HotelTest {
+    public static void main(String[] args) {
+        HotelService hotelService = new HotelService();
+        List<Hotel> hotels = hotelService.getAllHotels();
+
+        System.out.println("호텔 개수: " + hotels.size());
+        for (Hotel h : hotels) {
+            System.out.println(h.getName() + " | " + h.getCity() + " | " + h.getPrice());
+        }
+    }
+}
